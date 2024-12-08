8b198833018af3dc65e4e5e0a2ebf20af6216a41
Yasamansepahvand <yasnqd@gmail.com>
2024-12-08 12:04:41 +0330

    Add files via upload

---
+++ Example2_1.java
@@ -0,0 +1,11 @@
+
+public class Example2_1 {
+   public static void main(String[] args)
+   {
+	   System.out.print("Java is a good language ");
+	   System.out.println("and it is my favorite.");
+	   System.out.println("This is my first program in Java.");
+	   System.out.println("I wrote it in Eclipse.");
+   
+   }
+}
